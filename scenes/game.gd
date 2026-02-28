ce2abba364d49150aadd83067dd1c11ea966a6d7
YoussefPro-698 <youssef.pro.coding@gmail.com>
2026-02-28 14:46:41 +0000

    aaaaaaaaa doen some stuff

---
+++ scenes/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+@onready var bg = $bg
+@onready var gb = $gb
+var speed = 200
+var height = 210
+
+func _process(delta: float) -> void:
+	# so uh, i think ill just do particules hehe
+	pass
